66bf66d48164b272c0c6c16c3a77c804ea9a87c4
Braden Shepherdson <braden.shepherdson@gmail.com>
2020-06-02 23:20:23 -0400

    Add Vue.js support, demonstrating a form sending and receiving

--- web/index.ts
+++ web/index.ts
@@ -1,19 +1,32 @@
 import {HelloRequest, HelloResponse} from 'tpt/proto/greeter_web_pb';
 import {GreeterClient} from 'tpt/proto/greeter_web_grpc_web_pb';
+import Vue from 'vue';
 
 // Note that this is a CORS request, talking to the proxy server rather than the
 // web server. This probably isn't the best production configuration, where you
 // probably want a central reverse proxy doing your load-balancing, and routing
 // requests to the web server or gRPC server as appropriate.
-const client = new GreeterClient('http://localhost:5080/api');
+const client = new GreeterClient('http://192.168.86.8:5080/api');
 
-const request = new HelloRequest();
-request.setName('World!');
 
-client.sayHello(request, {}, (err, resp) => {
-  if (err) {
-    console.error('gRPC error', err);
-  } else {
-    console.log(resp.getMessage());
-  }
+const app = new Vue({
+  el: '#app',
+  data: {
+    name: 'Vue',
+    result: '',
+  },
+  methods: {
+    send: function() {
+      const req = new HelloRequest();
+      req.setName(this.name);
+      client.sayHello(req, {}, (err, resp) => {
+        if (err) {
+          console.error('gRPC error', err);
+        } else {
+          this.result = resp.getMessage();
+        }
+      });
+    },
+  },
 });
+
